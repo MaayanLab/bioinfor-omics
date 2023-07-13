01f3cb611de3bdc33f1fa5ace0c25f9a8be6a399
Nooha Kawsar <135756625+nkawsar10@users.noreply.github.com>
2023-07-13 10:49:31 -0400

    Quiz added

---
+++ quiz/extension/extension.js
@@ -0,0 +1,11 @@
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", redirect);
+
+
+function redirect(){
+    window.location.replace('http://127.0.0.1:5500/quiz/index.html');
+}
+
+
